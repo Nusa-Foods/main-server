f4116f10718bde82dd34c99a01587e093c41b7cb
putram11 <putram.works@gmail.com>
2024-09-03 11:41:14 +0700

    updated User

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const routes = require("./routes");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
-// app.use(bodyParser.json());
-
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
