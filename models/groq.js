e7fced01dccff4ef63b8c8b2550e6a1e8f5a0518
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-09-05 02:59:06 +0700

    fix: ai config

--- models/groq.js
+++ models/groq.js
@@ -5,17 +5,38 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 // const model = "gemma2-9b-it"; // google
 const model = "mixtral-8x7b-32768"; // mixtral
 
-const recipeGenerator = async (ingredients) => {
-    const response = await groq.chat.completions.create({
-        messages: [
-            {
-                role: "user",
-                content: `buatkan saya satu resep makanan yang menggunakan bahan bahan ini, bahan makanan = ${ingredients}. gunakan bahasa indonesia`,
-            },
-        ],
-        model: model,
-    });
-    return response.choices[0].message.content;
+const recipeGenerator = async (ingredients, retries = 3) => {
+    for (let attempt = 0; attempt < retries; attempt++) {
+        try {
+            const response = await groq.chat.completions.create({
+                messages: [
+                    {
+                        role: "user",
+                        content: `{\n"name": "recipe name",\n"ingredients": [[\'ingredients dose\', 'ingredients name']],\n"guide": "guide to cooks"\n}\n\ngive me that json format for recipe that user ${ingredients}`,
+                    },
+                ],
+                model: "llama3-8b-8192",
+                temperature: 1,
+                max_tokens: 1024,
+                top_p: 1,
+                stream: false,
+                response_format: {
+                    type: "json_object",
+                },
+                stop: null,
+            });
+
+            // If the response is successful, parse and return it
+            return JSON.parse(response.choices[0].message.content);
+        } catch (error) {
+            console.error(`Attempt ${attempt + 1} failed:`, error);
+            if (attempt === retries - 1) {
+                throw new Error(
+                    "Failed to generate recipe after multiple attempts."
+                );
+            }
+        }
+    }
 };
 
 const ingredientDescriptionGenerator = async (ingredients) => {
