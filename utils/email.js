1b19245f911d7583a2119ef895912d1136c1bf0a
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-09-09 05:34:20 +0700

    feat: add emailjs to handle email registration notification

---
+++ utils/email.js
@@ -0,0 +1,23 @@
+global.XMLHttpRequest = require("xhr2");
+const emailjs = require("emailjs-com");
+
+async function sendMail(email) {
+    try {
+        await emailjs.send(
+            process.env.EMAIL_SERVICE,
+            process.env.EMAIL_TEMPLATE,
+            {
+                to_name: email,
+                to_email: email,
+                message: `Congratz your email [ ${email} ] has been registered on Nusa Foods. start your cheff journey NOW!`,
+            },
+            process.env.EMAIL_API
+        );
+        console.log("SUCCESS SENDING EMAIL CREATION!");
+    } catch (err) {
+        console.log("ERROR", err);
+    }
+}
+module.exports = {
+    sendMail,
+};
