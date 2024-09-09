9065fc859e35f0e3cca4161ffe72383a889261b7
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2024-09-09 22:30:01 +0700

    fix: cors handling more than 1 site

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const routes = require("./routes");
 const cors = require("cors");
-
-// app.use(
-//     cors({
-//         origin: "http://localhost:3001",
-//         credentials: true,
-//         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//         allowedHeaders: ["Authorization", "Content-Type", "x-email", "x-id"],
-//     })
-// );
+const allowedDomain = [process.env.CLIENT_DOMAIN, process.env.ADMIN_DOMAIN];
 
 app.use(
     cors({
-        origin: "*",
+        origin: allowedDomain,
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Authorization", "Content-Type", "x-email", "x-id"],
@@ -24,9 +15,6 @@ app.use(
 );
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
-
 module.exports = app;
